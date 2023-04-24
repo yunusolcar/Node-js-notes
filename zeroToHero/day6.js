c8fdf855d57a2044b749748d090b9434ef346c5f
Yunus <yunusolcar@hotmail.com>
2023-04-24 12:08:37 +0300

    day6 node.js

--- zeroToHero/day6.js
+++ zeroToHero/day6.js
@@ -19,4 +19,4 @@ console.log(process.argv); //işlemlerle ilgili bir array döndürür.
 //Bu array programı çalıştıran kullanılan parametreleri döndürür.
 //console.log(process.argv.slice(2));
 
-primeNumbers(arguments[0] * 1, arguments[1] * 1);
+primeNumbers(arguments[0] * 1, arguments[1] * 1); 
