8f12718d6e191755e3110f75bf924198e08819d9
Yunus <yunusolcar@hotmail.com>
2023-04-25 16:54:18 +0300

    day7 node.js

--- zeroToHero/day7.js
+++ zeroToHero/day7.js
@@ -4,9 +4,17 @@
 Bazı fonksiyonlar tarayıcıda çalıştırılabilir
 fakat başka çalışma ortamlarında çalıştırılamaz.
  Örn: alert çalıştırılamaz*/
- 
- console.log('information from DAY7.JS');
 
- //node.js ile .load ile dosya çalıştırılabilir
- // path C:\Node-js-notes>node ./zeroToHero/day7.js
- //result: "information from DAY7.JS"
+console.log('information from DAY7.JS');
+
+//node.js ile .load ile dosya çalıştırılabilir
+// path C:\Node-js-notes>node ./zeroToHero/day7.js
+//result: "information from DAY7.JS"
+
+
+
+     
+//Node.js Asenkron Programlama - Asynchronous Programming
+
+/*Asenkron programlama ise uzun süren bir işlemi beklemeden diğer işlemlere devam edebilmektir, 
+işlemlerin sırasıyla devam etme zorunluluğu yoktur. Bir işlem yapılırken diğer işlemler bloklanmaz.*/
