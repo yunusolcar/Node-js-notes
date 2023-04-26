0837b91899b80bffe12b16218e30cf466996a1b8
Yunus <yunusolcar@hotmail.com>
2023-04-26 11:01:34 +0300

    day8-9 node.js

--- zeroToHero/day8.js
+++ zeroToHero/day8.js
@@ -1,5 +1,4 @@
 // Node.js  - Callback
-
 const function1 = () => {
      console.log("Function 1 working");
      function3();
@@ -14,14 +13,66 @@ const function3 = () => {
 };
 
 function1();
-
-
 /* Result
 Function 1 working
 Function 3 working
 Function 2 working
 */
 
-
 //Callback ile function içinde function çağırabildik.
 //Senkron olarak çalıştı. Çünkü js single thread.
+
+
+const books = [{
+          name: 'Book 1',
+          author: 'Author A'
+     }, {
+          name: 'Book 2',
+          author: 'Author B'
+     }, {
+          name: 'Book 3',
+          author: 'Author C'
+     }
+
+];
+
+const listBooks = () => {
+     books.map(book => {
+          console.log(book.name);
+     })
+};
+
+//listBooks();
+/* Result
+Book 1
+Book 2
+Book 3 */
+
+const addBook = (newBook) => {
+     books.push(newBook);
+};
+
+addBook({
+     name: 'Book 4',
+     author: 'Author D'
+});
+console.log(books);
+
+listBooks();
+/*Result
+Book 1
+Book 2
+Book 3
+Book 4 */
+
+
+// w/callback
+const addBock = (newBook, callback) => {
+     books.push(newBook);
+     callback();
+};
+
+addBock({
+     name: 'Book 4',
+     author: 'Author D'
+}, listBooks);
