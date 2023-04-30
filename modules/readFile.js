8f86a7aa600ff5add883cd4352a736abb73c1b1b
Yunus <yunusolcar@hotmail.com>
2023-04-30 14:01:02 +0300

    day11 File System Modules

---
+++ modules/readFile.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+//Dosya Okuma - Read File
+fs.readFile('password.txt', 'utf8', (err, data) => {
+     if (err) console.log(err);
+     console.log(data);
+     console.log('Dosya Okundu');
+});
+
+//Dosya Yazmak - Write File
+fs.writeFile('username.json', ' {"name": "Yunus", "surname": "Xxx"}',
+     'utf8', (err) => {
+          if (err) console.log(err);
+          console.log('Dosya başarıyla oluşturuldu');
+     });
+
+//Dosyaya Veri Ekleme - Add data to file
+fs.appendFile('password.txt', '\n New Secret Password :D', 'utf8', err => {
+     if (err) console.log(err);
+     console.log('Dosyaya veri başarıyla eklendi');
+});
+
+//Dosyadan Veri Silme - Delete data to file
+fs.unlink('username.json', err => {
+     if (err) console.log(err);
+     console.log('Dosya başarıyla silindi');
+});
+
+//Klasör Oluşturmak
+fs.mkdir('uploads/img', {
+     recursive: true
+}, err => {
+     if (err) console.log(err);
+     console.log('Klasör başarıyla oluşturuldu');
+});
+
+//Klasör Silme
+fs.rmdir('uploads', {
+     recursive: true
+}, err => {
+     if (err) console.log(err);
+     console.log('Klasör başarıyla silindi');
+});
