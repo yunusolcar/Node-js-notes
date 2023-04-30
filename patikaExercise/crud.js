1bdc06524f27ef0a4498b882d25446818682776d
Yunus <yunusolcar@hotmail.com>
2023-04-30 20:51:38 +0300

    patika.dev Exercise

---
+++ patikaExercise/crud.js
@@ -0,0 +1,23 @@
+//Patika.dev Create-Read-Update-Delete Example
+const fs = require('fs');
+
+fs.writeFile('employees.json', '{"name": "Employee 1  Iggy", "salary": 2000}',
+     'utf8', (err) => {
+          if (err) console.log(err);
+          console.log('OK');
+     });
+
+fs.readFile('employees.json', 'utf8', err => {
+     if (err) console.log(err);
+     console.log('File readed');
+});
+
+fs.appendFile('employees.json', '\n {"name": "Employee 1 : Yunus", "salary": 3000}', 'utf8', err => {
+     if (err) console.log(err);
+     console.log('Updated');
+});
+
+fs.unlink('employees.json', err => {
+     if (err) console.log(err);
+     console.log('File deleted');
+});
