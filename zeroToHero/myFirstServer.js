7bba09103666606f248d470ac07272c17470d577
Yunus <yunusolcar@hotmail.com>
2023-05-02 17:30:29 +0300

    day13 - patika.dev myServer Exercise

--- zeroToHero/myFirstServer.js
+++ zeroToHero/myFirstServer.js
@@ -7,25 +7,25 @@ const myServer = http.createServer((req, res) => {
           res.writeHead(200, {
                'Content-Type': 'text/html'
           });
-          res.write('<h1>Index Sayfasi</h1>');
+          res.write('<h2>Welcome to Index Page</h2>');
      } else if (myUrl === '/about') {
           res.writeHead(200, {
                'Content-Type': 'text/html'
           });
-          res.write('About Me Page');
+          res.write('<h2>Welcome to About Me Page</h2>');
      } else if (myUrl === '/contact') {
           res.writeHead(200, {
                'Content-Type': 'text/html'
           });
-          res.write('Contact Me Page');
+          res.write('<h2>Welcome to Contact Me Page</h2>');
      } else {
           res.writeHead(404, {
                'Content-Type': 'text/html'
           });
-          res.write('404 Page Not Found');
+          res.write('<h2>404 Page Not Found</h3>');
      }
 });
 
-myServer.listen(3000, () => {
-     console.log(`Server 3000 portundan başlatıldı`);
+myServer.listen(5000, () => {
+     console.log(`Server 5000 portundan başlatıldı`);
 });
