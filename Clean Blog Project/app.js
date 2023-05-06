818e246dae724c790c0fb0c663cf3077eb7efa39
Yunus <yunusolcar@hotmail.com>
2023-05-06 13:13:38 +0300

    Clean Blog Project

--- Clean Blog Project/app.js
+++ Clean Blog Project/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const path = require('path');
 const newPort = 3000;
 const app = express();
 
-app.get('/', (request, response) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  response.send(blog);
-});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
 app.listen(newPort, () => {
   console.log(`Server ${newPort} portu çalışıyor`);
 });
