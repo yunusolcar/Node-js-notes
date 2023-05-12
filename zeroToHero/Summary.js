e1e07b26683877e63e40dec51c3a8587445802dd
Yunus <yunusolcar@hotmail.com>
2023-05-12 23:40:05 +0300

    Node.js Summary

---
+++ zeroToHero/Summary.js
@@ -0,0 +1,14 @@
+/*
+Node.js Genel Özet - Notlarım
+
+     Event Driven - Olay Odaklı
+          Sistemdeki bir olay tetiklendiğinde sistemin diğer parçalarını ilgilendirmeksizin sadece ilgili olaydaki kodların çalışmasıdır.
+          Yani asenkron bir şekilde çalışmaktadır.
+     
+     Non Blocking
+          Node.js bir işlem sırasındaki olayın tamamlamasını beklemeden diğer olayı işleme almasıdır.
+
+     Asenkron
+          Kod akışının yukarıdan aşağıya ilerlemediği, işlemlerin birbirini beklemediği çalışma türüdür.
+
+*/
