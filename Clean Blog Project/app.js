2009e5d021260c406594bf0ca43648617cd28097
Yunus <yunusolcar@hotmail.com>
2023-05-26 21:48:40 +0300

    updates

--- Clean Blog Project/app.js
+++ Clean Blog Project/app.js
@@ -39,6 +39,6 @@ app.get('/add_post', pageController.getAddPage);
 app.get('/posts/edit/:id', pageController.getEditPage);
 
 //Port
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
